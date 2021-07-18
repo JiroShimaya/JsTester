6fc6f79e6e0e0b81d47f4c2d6fd6e2c20bbce2b7
jiro <jiro@192.168.11.21>
2021-07-18 14:08:13 +0900

    first commit

--- js/Tester.js
+++ js/Tester.js
@@ -1,9 +1,15 @@
 function Tester(){
 	const _this = {}
 
-	_this.exec = function(func, args, expected){
+	_this.exec = function(_func, _args, _expected){
+		const _arguments = [].slice.call(arguments);
+		
+		const func = _arguments[0];
+		const args = _arguments.slice(1, -1);
+		const expected = _arguments[_arguments.length-1];
+		
 		const value = func(...args);
-		console.log(args,value, expected);
+		//console.log(value, args,expected);
 		if(_this.isSame(value, expected)){
 			return true;
 		}
@@ -15,12 +21,12 @@ function Tester(){
 	_this.testAll = function(func, data){
 		let ok = true;
 		for(let d of data){
-			const args = d[0];
+			const args = d.slice(0,-1);
 			const value = func(...args);
-			const expected = d[1];
+			const expected = d[d.length-1];
 			
 			if(_this.isSame(value, expected)){
-				console.log("OK: args=",JSON.stringify(args));
+				console.log("OK: args=",JSON.stringify(args), JSON.stringify(value), "=",JSON.stringify(expected));
 			}
 			else{
 				console.log("NO: args=",JSON.stringify(args), JSON.stringify(value), "!=",JSON.stringify(expected));
@@ -43,18 +49,18 @@ function Tester(){
 		if(["undefined","null","boolean","number","string"].includes(type1)){
 			return object1 == object2;
 		}
-		else if(type1 == "array"){
+		else if(type1 === "array"){
 			if(object1.length != object2.length)return false;
 			for(let i=0;i<object1.length;i++){
-				if(!isSameObject(object1[i], object2[i]))return false;
+				if(_this.isSame(object1[i], object2[i]) === false)return false;
 			}
 			return true;
 		}
-		else if(type1 == "object"){
+		else if(type1 === "object"){
 			if(object1.length != object2.length)return false;
 			for(let k in object1){
 				if(k in object2 == false) return false;
-				if(!isSameObject(object1[k], object2[k]))return false;
+				if(_this.isSame(object1[k], object2[k]) === false)return false;
 			}
 			return true;
 		}
@@ -68,8 +74,6 @@ function Tester(){
 		exec: _this.exec,
 		testAll: _this.testAll
 	}
-
-	
 }
 
 export { Tester }
